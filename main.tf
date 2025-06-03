4e659062f171f4cf7f708014f6e480e1e59ee53a
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-03 14:21:43 +0400

    Refactor service account role attachment to improve readability and maintain structure

--- main.tf
+++ main.tf
@@ -45,16 +45,10 @@ resource "keycloak_openid_client" "this" {
   web_origins                                = ["/*"]
 }
 
-data "keycloak_openid_client" "client_data" {
-  realm_id  = var.realm_id
-  client_id = var.client_id
-  depends_on = [keycloak_openid_client.this]
-}
-
 # Attach roles to the service account
 resource "keycloak_openid_client_service_account_realm_role" "this" {
-  for_each                 = length(var.roles) > 0 ? toset(var.roles) : []
-  realm_id                 = var.realm_id
-  service_account_user_id  = data.keycloak_openid_client.client_data.service_account_user_id
-  role                     = each.value
+  for_each                = length(var.roles) > 0 ? toset(var.roles) : []
+  realm_id                = var.realm_id
+  service_account_user_id = data.keycloak_openid_client.client_data.service_account_user_id
+  role                    = each.value
 }
