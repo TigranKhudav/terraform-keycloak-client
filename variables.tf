ffab7901506ecf8b01b0be67c30997c9d5db4131
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-09 13:32:51 +0400

    Refactor keycloak_openid_client resource to utilize variables for configuration options and add missing variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -16,16 +16,126 @@ variable "client_secret" {
   type        = string
   default     = null
 }
+variable "access_token_lifespan" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "admin_url" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "always_display_in_console" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
+variable "backchannel_logout_revoke_offline_sessions" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
+variable "backchannel_logout_session_required" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = true
+}
+variable "backchannel_logout_url" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "base_url" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "client_offline_session_idle_timeout" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "client_offline_session_max_lifespan" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "client_session_idle_timeout" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "client_session_max_lifespan" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "consent_required" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
+variable "consent_screen_text" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "description" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
 variable "direct_access_grants_enabled" {
   description = "Keycloak client secret for Kafka"
   type        = bool
   default     = false
 }
-
+variable "display_on_consent_screen" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
+variable "client_enabled" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
 variable "frontchannel_logout_enabled" {
   description = "Keycloak client secret for Kafka"
   type        = bool
   default     = true
+}
+variable "exclude_issuer_from_auth_response" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "frontchannel_logout_url" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
+}
+variable "full_scope_allowed" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = true
+}
+variable "implicit_flow_enabled" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = true
+}
+variable "import" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = true
+}
+
+variable "exclude_session_state_from_auth_response" {
+  description = "Keycloak client secret for Kafka"
+  type        = string
+  default     = null
 }
 variable "oauth2_device_authorization_grant_enabled" {
   description = "Keycloak client secret for Kafka"
@@ -33,6 +143,11 @@ variable "oauth2_device_authorization_grant_enabled" {
   default     = false
 }
 variable "use_refresh_tokens" {
+  description = "Keycloak client secret for Kafka"
+  type        = bool
+  default     = false
+}
+variable "use_refresh_tokens_client_credentials" {
   description = "Keycloak client secret for Kafka"
   type        = bool
   default     = false
