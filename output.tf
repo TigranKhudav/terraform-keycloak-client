1cc71d52d971b98311c25edaf3cd85e51440aa5e
Tigran Khudaverdyan <tigran@Tigrans-MacBook-Pro.local>
2025-06-20 15:31:38 +0400

    Add output for role_id to output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "client_id" {
   value       = keycloak_openid_client.this.id
   description = "The ID of the S3 bucket"
 }
+output "role_id" {
+  value       = keycloak_role.this.id
+  description = "The ID of the S3 bucket"
+}
